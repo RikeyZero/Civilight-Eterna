7d3ca924eb8ee44603254da82aaeae642fddd7f4
Rice Shower <83920026+RikeyZero@users.noreply.github.com>
2024-05-22 13:39:15 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -69,7 +69,7 @@ LaoTeVoice=[
 async def handle_function():
     await LaoTe.send(random.choices(LaoTeVoice))
 
-LaoYa = on_regex(pattern=r"^超级?(小|牢)?特$", rule=to_me, block=True)
+LaoYa = on_regex(pattern=r"^Set?Your?Key?word?here$", rule=to_me, block=True)
 
 # 指定图片文件的路径
 img_path = Path(os.path.join(os.path.dirname(__file__), "Set your imgfile here"))
